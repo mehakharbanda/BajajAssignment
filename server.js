3dde60317951659b1a9d6cd0d5cec5407c1ff3bc
mehakharbanda <122426669+mehakharbanda@users.noreply.github.com>
2025-07-29 20:48:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT=process.env.PORT||3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/bfhl', bfhlRoute);
+app.use('/bfhl',bfhlRoute);
 
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`);
